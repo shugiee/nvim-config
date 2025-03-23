11ac04fbe9dfc2be68f9042476fec97f5bd8ea89
Jonathan Olson <62571810+jonathan-codebase@users.noreply.github.com>
2025-03-23 11:08:09 -0400

    Update harpoon's syntax to use v2 patterns

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,16 +1,16 @@
-require("harpoon").setup({
-    menu = {
-        width = vim.api.nvim_win_get_width(0) - 4,
-    }
+local harpoon = require("harpoon")
+
+harpoon:setup({
+  menu = {
+    width = vim.api.nvim_win_get_width(0) - 4,
+  }
 })
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
-vim.keymap.set("n", "<C-;>", function() ui.nav_file(5) end)
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-;>", function() harpoon:list():select(5) end)
