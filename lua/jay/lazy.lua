ed96ef923e5869885896be3707a4090d0f20e542
Jonathan Olson <62571810+jonathan-codebase@users.noreply.github.com>
2025-06-03 16:51:57 -0400

    Fix zen mode config, window width

--- lua/jay/lazy.lua
+++ lua/jay/lazy.lua
@@ -83,11 +83,9 @@ require("lazy").setup({
     {
         "folke/zen-mode.nvim",
         opts = {
-            {
-                window = {
-                    width = 150, -- width of the Zen window
-                },
-            }
+            window = {
+                width = 200,
+            },
         }
     },
 
